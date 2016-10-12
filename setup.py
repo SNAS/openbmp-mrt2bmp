82d66e42d131b6371a10e10af269f5ef3c9b0abf
omerpalaz <omer.palaz@gmail.com>
2016-10-12 10:43:02 -0700

    Project directory structure is created

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='openbmp-mrt',
+      version='0.1.0',
+      description='OpenBMP MRT',
+      author='Tim Evens',
+      author_email='tim@openbmp.org',
+      url='',
+      data_files=[('etc', ['src/etc/openbmp-mrt.yml'])],
+      package_dir={'openbmp': 'src/site-packages/openbmp', 'openbmp.mrt': 'src/site-packages/openbmp/mrt'},
+      packages=['openbmp', 'openbmp.mrt'],
+      scripts=['src/bin/openbmp-mrt']
+     )
